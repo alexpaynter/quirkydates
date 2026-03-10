302bd9092601d2b04e78eace180154459fd947c2
Alex Paynter <alexcpaynter@gmail.com>
2026-03-10 12:28:50 -0700

    Just fixed some incomplete lines before uploading

--- R/quirkyspec.R
+++ R/quirkyspec.R
@@ -1,6 +1,10 @@
 new_quirkyspec <- function(x = list()) {
   stopifnot(is.list(x))
-  stopifnot(!is.null(names(x))
+  stopifnot(!is.null(names(x)))
   stopifnot(names(x) %in% units_list())
   structure(x, class = 'quirkyspec')
+}
+
+valid_quirky_units <- function() {
+  c('second', 'fornight', 'lunar_month', 'eon', 'scaramucci', 'dog_year')
 }
